d1c5027c9f5537c8c0040bbb49ec4d1b639a420a
Brandon Maldonado Alonso <brandonma98@protonmail.com>
2020-06-07 00:22:35 -0500

    Moved example project to a different repository

--- ExampleProject/firebase.ts
+++
@@ -1,9 +0,0 @@
-export default {
-    apiKey: 'AIzaSyB8N_HIoF701Pu6omhY-_tpcDkXEvvgzZY',
-    authDomain: 'fir-db-manager.firebaseapp.com',
-    databaseURL: 'https://fir-db-manager.firebaseio.com',
-    projectId: 'fir-db-manager',
-    storageBucket: 'fir-db-manager.appspot.com',
-    messagingSenderId: '215089057587',
-    appId: '1:215089057587:web:166a4e5ab680ba6caab846'
-};
