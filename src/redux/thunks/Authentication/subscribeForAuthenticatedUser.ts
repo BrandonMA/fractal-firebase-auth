3f86a8454ff1b8641181f88f14935b24f595b68c
Brandon Maldonado Alonso <brandonma98@protonmail.com>
2020-08-04 16:22:33 -0500

    migration to isolated functions

--- src/redux/thunks/Authentication/subscribeForAuthenticatedUser.ts
+++ src/redux/thunks/Authentication/subscribeForAuthenticatedUser.ts
@@ -1,24 +1,17 @@
 import * as firebase from 'firebase/app';
 import 'firebase/auth';
-import { ThunkDispatch } from 'redux-thunk';
-import { Action } from 'redux';
-import { MinimalExpectedReduxState } from '../..';
-import { authenticationSlice } from '../../slices';
+import { AuthenticationState } from '../..';
 
-export const subscribeForAuthenticatedUser = (slice: typeof authenticationSlice) => (
-    dispatch: ThunkDispatch<firebase.Unsubscribe, MinimalExpectedReduxState, Action>
-): firebase.Unsubscribe => {
+export function subscribeForAuthenticatedUser(onFetch: (state: AuthenticationState) => void): firebase.Unsubscribe {
     return firebase.auth().onAuthStateChanged(
         (user) => {
-            dispatch(
-                slice.actions.setAuthenticationState({
-                    firebaseUser: user,
-                    loading: false
-                })
-            );
+            onFetch({
+                firebaseUser: user,
+                loading: false
+            });
         },
         (error) => {
             alert(error.message);
         }
     );
-};
+}
