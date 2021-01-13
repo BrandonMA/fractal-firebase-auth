32ef163e8737b42625cc700f0a65902a5633debb
BrandonMA <brandonma98@protonmail.com>
2021-01-13 16:15:49 -0600

    added a way to customize the object that is going to be saved on firebase, before actually saving it

--- dist/components/screens/CreateUserScreen.d.ts
+++ dist/components/screens/CreateUserScreen.d.ts
@@ -2,5 +2,6 @@
 import { MinimalExpectedDatabase, MinimalUserData } from '../../types';
 export interface CreateUserScreenProps {
     database: MinimalExpectedDatabase<MinimalUserData, unknown>;
+    createUserObject: (basicUser: MinimalUserData) => any;
 }
-export declare function CreateUserScreen({ database }: CreateUserScreenProps): JSX.Element;
+export declare function CreateUserScreen({ database, createUserObject }: CreateUserScreenProps): JSX.Element;
--- dist/components/screens/CreateUserScreen.js
+++ dist/components/screens/CreateUserScreen.js
@@ -3,17 +3,18 @@ import React, { useEffect } from 'react';
 import { createUser } from '../../firebase/users/createUser';
 import { useAuthenticationState } from '../../hooks/useAuthenticationState';
 export function CreateUserScreen(_a) {
-    var database = _a.database;
+    var database = _a.database, createUserObject = _a.createUserObject;
     var authenticationState = useAuthenticationState();
     useEffect(function () {
         var _a;
         if (authenticationState.firebaseUser != null) {
-            createUser(database, {
+            var finalUser = createUserObject({
                 email: (_a = authenticationState.firebaseUser.email) !== null && _a !== void 0 ? _a : '',
                 id: authenticationState.firebaseUser.uid
-            })["catch"](function (error) { return alert(error.message); });
+            });
+            createUser(database, finalUser)["catch"](function (error) { return alert(error.message); });
         }
-    }, [database, authenticationState]);
+    }, [database, authenticationState, createUserObject]);
     return React.createElement(LoadingBackground, null);
 }
 //# sourceMappingURL=CreateUserScreen.js.map
