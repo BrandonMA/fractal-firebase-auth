ca90c6bae2a8b85217f2299f0b27f3bdc99cf482
BrandonMA <brandonma98@protonmail.com>
2021-06-03 20:10:43 +0900

    removed memo because it was canceling re renders

--- dist/hooks/useAuthenticateChildren.js
+++ dist/hooks/useAuthenticateChildren.js
@@ -1,55 +1,53 @@
 import { AuthenticateChildrenKey } from '../types';
-import React, { Fragment, useMemo } from 'react';
+import React, { Fragment } from 'react';
 export function useAuthenticateChildren(children) {
-    return useMemo(function () {
-        var AppComponent = {
-            component: React.createElement(Fragment, null),
-            route: 'app'
-        };
-        var LoadingComponent = {
-            component: React.createElement(Fragment, null),
-            route: 'loading'
-        };
-        var AuthenticationComponent = {
-            component: React.createElement(Fragment, null),
-            route: 'authenticate'
-        };
-        var CreateUserComponent = {
-            component: React.createElement(Fragment, null),
-            route: 'create_user'
-        };
-        React.Children.forEach(children, function (child) {
-            var props = child.props;
-            switch (child.key) {
-                case AuthenticateChildrenKey.AppContent:
-                    AppComponent = {
-                        component: child,
-                        route: props.route
-                    };
-                    break;
-                case AuthenticateChildrenKey.Loading:
-                    LoadingComponent = {
-                        component: child,
-                        route: props.route
-                    };
-                    break;
-                case AuthenticateChildrenKey.Authentication:
-                    AuthenticationComponent = {
-                        component: child,
-                        route: props.route
-                    };
-                    break;
-                case AuthenticateChildrenKey.CreateUser:
-                    CreateUserComponent = {
-                        component: child,
-                        route: props.route
-                    };
-                    break;
-                default:
-                    break;
-            }
-        });
-        return [AppComponent, LoadingComponent, AuthenticationComponent, CreateUserComponent];
-    }, [children]);
+    var AppComponent = {
+        component: React.createElement(Fragment, null),
+        route: 'app'
+    };
+    var LoadingComponent = {
+        component: React.createElement(Fragment, null),
+        route: 'loading'
+    };
+    var AuthenticationComponent = {
+        component: React.createElement(Fragment, null),
+        route: 'authenticate'
+    };
+    var CreateUserComponent = {
+        component: React.createElement(Fragment, null),
+        route: 'create_user'
+    };
+    React.Children.forEach(children, function (child) {
+        var props = child.props;
+        switch (child.key) {
+            case AuthenticateChildrenKey.AppContent:
+                AppComponent = {
+                    component: child,
+                    route: props.route
+                };
+                break;
+            case AuthenticateChildrenKey.Loading:
+                LoadingComponent = {
+                    component: child,
+                    route: props.route
+                };
+                break;
+            case AuthenticateChildrenKey.Authentication:
+                AuthenticationComponent = {
+                    component: child,
+                    route: props.route
+                };
+                break;
+            case AuthenticateChildrenKey.CreateUser:
+                CreateUserComponent = {
+                    component: child,
+                    route: props.route
+                };
+                break;
+            default:
+                break;
+        }
+    });
+    return [AppComponent, LoadingComponent, AuthenticationComponent, CreateUserComponent];
 }
 //# sourceMappingURL=useAuthenticateChildren.js.map
