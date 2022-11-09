901395f91aa379c9dbc37e6587a4ed8a4861a9d7
baltazar-gr <69155714+baltazar-gr@users.noreply.github.com>
2022-11-09 13:58:35 -0600

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -14,12 +14,12 @@ import {
     resetPassword
 } from './src';
 import { Collection, Database, IDEnabled } from '@bma98/firebase-db-manager';
-import { NavigationRouter } from '@bma98/fractal-navigation-router';
+import { NavigationBarConfig, NavigationRouter, StackNavigator, StackScreen, useNavigate } from '@bma98/fractal-navigation-router';
 import { Button, LoadingBackground, PaddingLayer, Text } from '@bma98/fractal-ui';
 import { Image, ScrollView } from 'react-native';
-import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import { AuthenticateChildrenKey } from './src/types/AuthenticateChildrenKey';
 import { AuthenticateSection } from './src/components/AuthenticateSection';
+import { NavigationBarInsetsLayer } from '@bma98/fractal-navigation';
 
 interface User extends IDEnabled {
     email: string;
@@ -36,21 +36,39 @@ function createDatabase(): DatabaseType {
     });
 }
 
-function Home(): JSX.Element {
+export function Home() {
     const { loading, firebaseUser } = useAuthenticationState();
     const currentUser = useUserDocument<User, null>();
+    const navigate = useNavigate();
+
+    const goToProfile = () => {
+        navigate('/app/profile');
+    };
 
     return (
-        <SafeAreaProvider>
-            <ScrollView>
-                <SafeAreaView />
-                <PaddingLayer>
-                    <Text marginBottom={12}>{`Logged In! ${currentUser?.data?.email}`}</Text>
-                    <Button marginBottom={12} text='Sign Out' onPress={signOut} />
-                    {!loading && firebaseUser != null ? <Text>{firebaseUser.uid}</Text> : <Text>Still getting the user</Text>}
-                </PaddingLayer>
-            </ScrollView>
-        </SafeAreaProvider>
+        <StackNavigator path='/app'>
+            <StackScreen path='/app' navBarConfig={<NavigationBarConfig title={'Home'} />}>
+                <NavigationBarInsetsLayer>
+                    <ScrollView>
+                        <PaddingLayer>
+                            <Text marginBottom={12}>{`Logged In! ${currentUser?.data?.email}`}</Text>
+                            <Button marginBottom={12} text='Sign Out' onPress={signOut} />
+                            <Button marginBottom={12} text='Go To Profile' onPress={goToProfile} />
+                            {!loading && firebaseUser != null ? <Text>{firebaseUser.uid}</Text> : <Text>Still getting the user</Text>}
+                        </PaddingLayer>
+                    </ScrollView>
+                </NavigationBarInsetsLayer>
+            </StackScreen>
+            <StackScreen path='/app/profile' navBarConfig={<NavigationBarConfig title={'Profile'} />}>
+                <NavigationBarInsetsLayer>
+                    <ScrollView>
+                        <PaddingLayer>
+                            <Text marginBottom={12}>User Profile</Text>
+                        </PaddingLayer>
+                    </ScrollView>
+                </NavigationBarInsetsLayer>
+            </StackScreen>
+        </StackNavigator>
     );
 }
 
