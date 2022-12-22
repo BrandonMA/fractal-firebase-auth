6d1e4dd90ec7b1cf606c6114f5f78ec263edb54a
Brandon MA <brandonma98@protonmail.com>
2022-12-22 16:50:35 -0600

    Add email scope to provider

--- src/firebase/authentication/facebook/signInWithPopup/index.ts
+++ src/firebase/authentication/facebook/signInWithPopup/index.ts
@@ -3,5 +3,6 @@ import 'firebase/compat/auth';
 
 export async function facebookSignInWithPopup(): Promise<unknown> {
     const provider = new firebase.auth.FacebookAuthProvider();
+    provider.addScope('email');
     return firebase.auth().signInWithPopup(provider);
 }
